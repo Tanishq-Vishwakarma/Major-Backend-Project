11644632a98c3dd49bb4588845058a1a66659cdc
Tanishq Vishwakarma <diveshvishwakarma71@gmail.com>
2025-08-02 22:45:39 +0530

    Utils files and code added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config();
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed !! ", error);
+})
 
 // import express from "express";
 // const app = express();
