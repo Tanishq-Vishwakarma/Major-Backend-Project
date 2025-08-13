bbc121e19d7439c2cbc260ace54f9b2c77a23bbf
Tanishq Vishwakarma <diveshvishwakarma71@gmail.com>
2025-08-13 20:09:39 +0530

    Add register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,8 +52,8 @@ userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) {
         return next();
     }
-    this.password = bcrypt.hash(this.password, 10)
-    next()
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
